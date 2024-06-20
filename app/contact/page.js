5f17ade51637cb94e811c9276b8e45e8b98c0974
FloreDenis2001 <floredenis907@yahoo.com>
2024-06-20 21:38:14 +0300

    last_update

--- app/contact/page.js
+++ app/contact/page.js
@@ -25,11 +25,24 @@ export default function Contact() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        const templateParams = {
+            from_name: formData.firstName + ' ' + formData.lastName,
+            to_name: 'Azonto Studio',
+            message: formData.message,
+            phone: formData.phone,
+            from_email: formData.email
+        }
+        emailjs.send('service_6251sxh', 'template_vzsyhoy', templateParams, 'xsYSJ0cfhSblt9LHP')
             .then((response) => {
                 console.log('Email sent successfully:', response);
                 toast.success('Mesajul a fost trimis cu succes!');
+                setFormData({
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    phone: '',
+                    message: ''
+                });
             })
             .catch((error) => {
                 console.error('Email sending failed:', error);
@@ -39,38 +52,38 @@ export default function Contact() {
     return (
         <div className="flex justify-center items-center  pt-20 bg-neutral-900">
             <div className="container mx-auto my-4 px-4 lg:px-20">
-            <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
-                <div className="flex">
-                    <h1 className="pt-10 font-bold font-manrope uppercase text-5xl text-white">Trimite un <br /> <span className="text-orange-500">mesaj</span> </h1>
+                <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
+                    <div className="flex">
+                        <h1 className="pt-10 font-bold font-manrope uppercase text-5xl text-white">Trimite un <br /> <span className="text-orange-500">mesaj</span> </h1>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5 font-manrope">
+                            <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                type="text" placeholder="First Name*" name="firstName" value={formData.firstName} onChange={handleChange} />
+                            <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                type="text" placeholder="Last Name*" name="lastName" value={formData.lastName} onChange={handleChange} />
+                            <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                type="email" placeholder="Email*" name="email" value={formData.email} onChange={handleChange} />
+                            <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                type="number" placeholder="Phone*" name="phone" value={formData.phone} onChange={handleChange} />
+                        </div>
+                        <div className="my-4">
+                            <textarea placeholder="Message*" className="font-manrope w-full h-32 bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" name="message" value={formData.message} onChange={handleChange}></textarea>
+                        </div>
+                        <div className="my-2 w-1/2 lg:w-1/4">
+                            <button type="submit" className="uppercase text-sm font-bold tracking-wide bg-orange-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+                                Send Message
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5 font-manrope">
-                        <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="text" placeholder="First Name*" name="firstName" value={formData.firstName} onChange={handleChange} />
-                        <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="text" placeholder="Last Name*" name="lastName" value={formData.lastName} onChange={handleChange} />
-                        <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="email" placeholder="Email*" name="email" value={formData.email} onChange={handleChange} />
-                        <input className="w-full bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="number" placeholder="Phone*" name="phone" value={formData.phone} onChange={handleChange} />
-                    </div>
-                    <div className="my-4">
-                        <textarea placeholder="Message*" className="font-manrope w-full h-32 bg-neutral-800 text-white mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" name="message" value={formData.message} onChange={handleChange}></textarea>
-                    </div>
-                    <div className="my-2 w-1/2 lg:w-1/4">
-                        <button type="submit" className="uppercase text-sm font-bold tracking-wide bg-orange-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
-                            Send Message
-                        </button>
-                    </div>
-                </form>
-            </div>
 
                 <div className="w-full lg:-mt-96 lg:w-2/6 px-8 py-12 ml-auto bg-orange-500 rounded-2xl">
                     <div className="flex flex-col text-white">
                         <h1 className="font-bold uppercase text-3xl my-4">Contactează-ne</h1>
                         <p className="text-gray-300">
-                            Te așteptăm cu drag 
-                             pentru a discuta despre cum putem imortaliza cele mai frumoase momente din viața ta. Fie că este vorba de portrete, evenimente speciale sau sesiuni foto creative, echipa noastră este pregătită să îți ofere servicii profesionale într-un mediu plăcut și relaxant.
+                            Te așteptăm cu drag
+                            pentru a discuta despre cum putem imortaliza cele mai frumoase momente din viața ta. Fie că este vorba de portrete, evenimente speciale sau sesiuni foto creative, echipa noastră este pregătită să îți ofere servicii profesionale într-un mediu plăcut și relaxant.
                         </p>
                         <div className="flex my-4 w-2/3 lg:w-full">
                             <div className="flex flex-col">
